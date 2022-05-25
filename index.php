345bab66117a384002e328db1af9089d14c1c968
serhiy <paz.serhiy@gmail.com>
2022-05-25 23:12:06 +0300

    Create php into project

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+    require_once("layout/head.php");
+    require_once("layout/header.php");
+
+    if(isset($_GET["action"])){
+        $a = $_GET["action"];
+        if (file_exists("views/".$a.".php")) {
+            require_once("views/".$a.".php");
+        }	else {
+            require_once("views/404.php");
+        }
+    }
+    else {
+        require_once("views/main.php");
+    }
+
+    require_once("layout/footer.php");
+?>
---
+++ views/main.php
@@ -0,0 +1,136 @@
+<main>
+    <section class="hello">
+        <div class="container">
+            <h1><span class="yellow">Rosava</span> <span class="green">Group</span></h1>
+            <h2>Efficient. Safe. Reliable.</h2>
+        </div>
+    </section>
+    <section class="about" id="about">
+        <div class="container">
+            <div class="info">
+                <h3 class="info__title">Rosava Group</h3> 
+                <p>GTS Transportation is a rapidly growing company located in the Chicagoland area operating in 48 states, with more than 350 trucks. </p>   
+                <div class="info__btns">
+                    <a href="#" class="full">FULL APPLICATION</a>
+                    <a href="#" class="quick">QUICK APPLY</a>
+                </div>
+                <div class="info__services">
+                    <a href="#">
+                        <img src="/img/assets/Solo.jpg" alt="Solo Driver">
+                        <h4>SOLO DRIVER</h4>
+                    </a>
+                    <a href="#">
+                        <img src="/img/assets/owner-operator.jpg" alt="Own. Operator">
+                        <h4>OWN. OPERATOR</h4>
+                    </a>
+                    <a href="#">
+                        <img src="/img/assets/team.jpg" alt="Team Drivers">
+                        <h4>TEAM DRIVERS</h4>
+                    </a>
+                </div>
+            </div>
+            <div class="photo">
+                <h3 class="photo__address">4612 Hirschberg Ave, Schiller Park, IL 60176, USA</h3>
+                <div class="photo__img"></div>
+                <div class="photo__img2">
+                    <h4>WELCOME TO ROSAVA GROUP</h4>
+                    <p>Be Part of a Great Team</p>  
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="teamWork">
+        <div class="container">
+            <div class="teamWork__photo"><img src="/img/assets/driver.jpg" alt="Team Work Photo"></div>
+            <div class="teamWork__data">
+                <h4 class="title">TEAM WORK</h4>
+                <h5 class="slogan1">WORK AS ONE</h5>
+                <h6 class="slogan2">EVERY GEAR COUNTS</h6>
+                <p>Each week, GTS looks to its truck drivers to nominate a best driver of the week. GTS offers opportunities for drivers to earn an <strong> additional $1,000 each month with safe driver reward programs.
+                    At the end of the year one lucky driver will win a family vacation to sunny FL! </strong>
+                    We recognize our drivers for their dedication to the profession and an excellent driving record!</p>
+            </div>
+        </div>
+    </section>
+    <section class="services">
+        <div class="container">
+            <div class="services__service" href="#">
+                <h4>JOIN</h4>
+                <h3>Solo Driver</h3>
+                <p>GTS Transportation is looking for solo drivers to fill some of our open trucks.</p>
+                <a href="#"><span class="left">❮</span> MORE INFO <span class="right">❯</span></a>
+            </div>
+            <div class="services__service" href="#">
+                <h4>US</h4>
+                <h3>Owner Operator</h3>
+                <p>Enjoy the freedom of your own authority while we provide all the support on the back end.</p>
+                <a href="#"><span class="left">❮</span> MORE INFO <span class="right">❯</span></a>
+            </div>
+            <div class="services__service" href="#">
+                <h4>TODAY</h4>
+                <h3>Team Drivers</h3>
+                <p>Teams of two are able to effectively manage their time and are able to run more miles than a solo driver.</p>
+                <a href="#"><span class="left">❮</span> MORE INFO <span class="right">❯</span></a>
+            </div>
+        </div>
+    </section>
+    <section class="applyToDrive">
+        <div class="container">
+            <div class="applyToDrive__form">
+                <h2>APPLY TO DRIVE FOR ROSAVA GROUP!</h2>
+                <form action="/" method="POST">
+                    <input type="text" maxlength="50" placeholder="Name" required name="name" id="name">
+
+                    <input type="email" placeholder="Email Address" required name="mail">
+
+                    <input type="tel" maxlength="20" placeholder="Phone Number" required name="tel">
+
+                    <input type="text" maxlength="50" placeholder="Zip Code" pattern="[0-9\s-]{4,6}" required name="code">
+
+                    <select name="services" id="services" required>
+                        <option value>I am applying for</option>
+                        <option value="Solo">Solo Driver</option>
+                        <option value="Owner">Owner Operator</option>
+                        <option value="Team">Team Drivers</option>
+                    </select>
+
+                    <select name="driving_experience" id="driving_experience" required>
+                        <option value="">CDL Driving Experience</option>
+                        <option value="No experience">No experience</option>
+                        <option value="Under 1 year">Under 1 year</option>
+                        <option value="More than 1 year">More than 1 year</option>
+                    </select>
+
+                    <select name="contact_about_us" id="contact_about_us" required>
+                        <option value="">How did you hear about us?</option>
+                        <option value="Google">Google</option>
+                        <option value="Facebook">Facebook</option>
+                        <option value="Craigslist">Craigslist</option>
+                        <option value="Referral">Referral</option>
+                        <option value="Other">Other</option>
+                    </select>
+
+                    <button type="submit">SUBMIT</button>
+                </form>
+            </div>
+            <div class="applyToDrive__info">
+                <h2>BRAND NEW <br> 2021-2022 TRUCKS</h2>
+                <h3>FREIGHTLINER, VOLVO, PETERBILT</h3>
+                <ul>
+                    <li>
+                        <h4>Dedicated Dispatch</h4>
+                    </li>
+                    <li>
+                        <h4>Dedicated Safety Personnel</h4>
+                    </li>
+                    <li>
+                        <h4>Maintenance Shop On-Site</h4>
+                    </li>
+                    <li>
+                        <h4>Healthcare, Dental, Vision and Life Insurance</h4>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </section>
+</main>
