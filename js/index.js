cf8c36ee3a497996e8ebc4a26f6a17ffeb65dfc1
serhiy <paz.serhiy@gmail.com>
2022-05-25 08:05:41 +0300

    Add some edits

--- js/index.js
+++ js/index.js
@@ -4,11 +4,11 @@ const headerBody = document.querySelector("header nav.body");
 const headerHeadHeight = document.querySelector("header nav.head").offsetHeight;
 
 window.addEventListener("scroll",function(){
-    if(Math.floor(this.window.scrollY) >= headerHeadHeight){
+    if(Math.floor(window.scrollY) >= headerHeadHeight){
         headerBody.style.position = "fixed"
         document.querySelector("main").style.marginTop = headerBody.offsetHeight+"px"
     }
-    if(Math.floor(this.window.scrollY) < headerHeadHeight){
+    if(Math.floor(window.scrollY) < headerHeadHeight){
         headerBody.style.position = "static"
         document.querySelector("main").style.marginTop = "0px"
     }
